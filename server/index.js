b5ce622f0a8e88bd32236b8a168093fe6dfceaf3
Amirun Nahar <naharamina68@gmail.com>
2025-08-02 21:13:36 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,7 +37,13 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/payment', paymentRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+const mongoURI = process.env.MONGODB_URI;
+if (!mongoURI) {
+  console.error('MONGODB_URI environment variable is not set!');
+  process.exit(1);
+}
+
+mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB Connection Error:', err));
 
